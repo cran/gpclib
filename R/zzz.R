b366854e178f68459a680096cff8cbe158400038
Roger D. Peng <rpeng@jhsph.edu>
2006-03-03 00:00:00 +0000

    version 1.3-3

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,12 @@
 .onLoad <- function(lib, pkg) {
-    if(!require(methods))
-        stop(sQuote("methods"), " package is required for ",
-             sQuote("gpclib"))
+    if(!require(methods, quietly = TRUE))
+        stop("'methods' package is required for 'gpclib'")
 }
 
 .onAttach <- function(lib, pkg) {
     ver <- read.dcf(file.path(lib,pkg,"DESCRIPTION"), "Version")
     msg <- paste("General Polygon Clipper Library for R (version ",
-                 as.character(ver), ")")
+                 as.character(ver), ")", sep = "")
     writeLines(strwrap(msg))
-    msg <- paste("Type", sQuote("class ? gpc.poly"), "for help")
-    writeLines(strwrap(msg, prefix = "\t"))
+    cat("\tType 'class ? gpc.poly' for help\n")
 }
